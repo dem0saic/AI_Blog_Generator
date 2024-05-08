66a34c08d9e1bc2523b2433e47b7d515ed86f50b
“dem0saic” <changeinculture4lyf@proton.me>
2024-05-08 00:08:00 +0000

    working on the sign page... allowing users to create a new account

--- AI_Blog_App/Blog_Generator/urls.py
+++ AI_Blog_App/Blog_Generator/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login', views.usr_login, name='login'),
+    path('signup', views.usr_signup, name='signup'),
+    path('logout', views.usr_logout, name='logout'),
 ]
